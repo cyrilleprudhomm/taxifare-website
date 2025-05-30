6d7142f3537d52c18eda900194d0ce71ddee6d95
cyrille prudhomme <cyrille.prudhomme@gmail.com>
2025-05-30 14:48:22 +0200

    plus joli

--- app.py
+++ app.py
@@ -1,48 +1,52 @@
 import streamlit as st
+import requests
+import numpy as np
 
 '''
-# TaxiFareModel front
+# Estimateur Taxifare
 '''
-
-st.markdown('''
-Remember that there are several ways to output content into your web page...
-
-Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
-''')
-
-'''
-## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
-
-1. Let's ask for:
-- date and time
-- pickup longitude
-- pickup latitude
-- dropoff longitude
-- dropoff latitude
-- passenger count
-'''
-
-'''
-## Once we have these, let's call our API in order to retrieve a prediction
-
-See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
-
-🤔 How could we call our API ? Off course... The `requests` package 💡
-'''
-
 url = 'https://taxifare.lewagon.ai/predict'
 
-if url == 'https://taxifare.lewagon.ai/predict':
+defaultdico = {
+        "pickup_datetime" : "2013-07-06 17:18:00",
+        "pickup_longitude":-73.950655,
+        "pickup_latitude":40.783282,
+        "dropoff_longitude":-73.984365,
+        "dropoff_latitude":40.769802,
+        "passenger_count":1
+    }
 
-    st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
+if "dico" not in st.session_state:
+    st.session_state['dico'] = defaultdico
 
-'''
+amount={'fare':0}
 
-2. Let's build a dictionary containing the parameters for our API...
+with st.form("my_form"):
+    '''## Où allons-nous ? 🚕'''
+    pickup_datetime = st.text_input("date and time", st.session_state['dico']['pickup_datetime'])
+    pickup_longitude = st.text_input("pickup longitude", st.session_state['dico']['pickup_longitude'])
+    pickup_latitude = st.text_input("pickup latitude", st.session_state['dico']['pickup_latitude'])
+    dropoff_longitude = st.text_input("dropoff longitude", st.session_state['dico']['dropoff_longitude'])
+    dropoff_latitude = st.text_input("dropoff latitude", st.session_state['dico']['dropoff_latitude'])
+    passenger_count = st.text_input("passenger count", st.session_state['dico']['passenger_count'])
 
-3. Let's call our API using the `requests` package...
-
-4. Let's retrieve the prediction from the **JSON** returned by the API...
-
-## Finally, we can display the prediction to the user
-'''
+    st.session_state['dico'] = {
+        "pickup_datetime" : pickup_datetime,
+        "pickup_longitude": pickup_longitude,
+        "pickup_latitude":pickup_latitude,
+        "dropoff_longitude":dropoff_longitude,
+        "dropoff_latitude":dropoff_latitude,
+        "passenger_count":passenger_count
+    }
+    if st.form_submit_button('Quel est le prix ?'):
+        r = requests.get(url, params=st.session_state['dico'])
+        amount = r.json()
+        st.markdown(f'''
+            ## 💵 prix de la course : {round(amount['fare'], 2)} $
+        ''')
+    else:
+        st.markdown(f'''
+            ## 💵 prix de la course : {0.00} $
+        ''')
+#if st.button("Reset", type="primary"):
+#    st.session_state['dico'] = defaultdico
